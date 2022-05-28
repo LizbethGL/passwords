2b43deba5f4a81c63c8e17bc685084e234707d47
lizgl <lizbethgl.ti19@utsjr.edu.mx>
2022-05-27 19:37:35 -0500

    entrega 3: pantalla registro

---
+++ app/src/main/java/com/example/logoapp/activity_registro.java
@@ -0,0 +1,18 @@
+package com.example.logoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class activity_registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+
+
+}
