afd5fe6d1985fe2fa81209e1d7f5d7c8202a200f
lizgl <lizbethgl.ti19@utsjr.edu.mx>
2022-05-31 10:28:46 -0500

    entrega 4: pantalla registro conectada a login

--- app/src/main/java/com/example/logoapp/Login.java
+++ app/src/main/java/com/example/logoapp/Login.java
@@ -2,13 +2,27 @@ package com.example.logoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textView = findViewById( R.id.btnRegisternow );
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this,activity_registro.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/logoapp/activity_registro.java
+++ app/src/main/java/com/example/logoapp/activity_registro.java
@@ -5,13 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class activity_registro extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+
+        textView = findViewById( R.id.login );
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity_registro.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
